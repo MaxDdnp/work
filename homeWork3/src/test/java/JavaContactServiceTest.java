830b11751903bef92b225c7e624f345bda616eb5
Max <maximapr@rambler.ru>
2015-03-24 21:57:04 +0200

    add some dependency

--- homeWork3/src/test/java/JavaContactServiceTest.java
+++ homeWork3/src/test/java/JavaContactServiceTest.java
@@ -2,11 +2,15 @@ import com.skillsup.daoInterface.implem.ContactDAO;
 import com.skillsup.service.implem.JavaContactService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+import java.time.LocalDate;
 
 
 /**
@@ -14,16 +18,23 @@ import javax.annotation.Resource;
  */
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @ContextConfiguration(locations = {"/test-context.xml"})
 public class JavaContactServiceTest {
-    @Resource(name = "contactDAO")
-    private ContactDAO contactDAO;
-    @Resource(name = "contactService")
+    @Mock
     private JavaContactService service;
+    @InjectMocks
+    private ContactDAO contactDAO = new ContactDAO();
+
+    private final String fstName = "Ivan";
+    private final String lstName = "Ivanov";
+    private final LocalDate date = LocalDate.of(1980, 06, 28);
+
 
     @Test
-    public void testContact() {
+    public void testCreateContact() {
+
+
 
     }
 }
