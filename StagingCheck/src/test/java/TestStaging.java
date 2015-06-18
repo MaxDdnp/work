b974b0a3c948e899a408aa7aa7f191bd881a3615
maximddnp <maximapr@yandex.ru>
2015-06-18 12:44:32 +0300

    add test for login

--- StagingCheck/src/test/java/TestStaging.java
+++ StagingCheck/src/test/java/TestStaging.java
@@ -3,6 +3,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.junit.*;
 
+import javax.validation.constraints.AssertFalse;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author Gaurang_Shah
  */
@@ -11,28 +14,48 @@ public class TestStaging {
     private String urlImage = "http://qa01.lmstaging.me/test2.html?ak=b4cc298cc3";
     private String urlVideo = "http://qa01.lmstaging.me/test2.html?ak=f5b90b66ed";
     private String urlHtml = "http://qa01.lmstaging.me/test2.html?ak=b664917b29";
+    private String urlLogin = "http://qa01.lmstaging.me/login";
+    private String cannotLoginMessage = "\n" +
+            "    We didn't recognize the username or password you entered. Please try again.\n" +
+            "  ";
 
-    @Test
-    public void testImage() throws InterruptedException {
+
+    @Before
+    public void createNewDriver() throws Exception {
         driver = new FirefoxDriver();
-        driver.get(urlImage);
-         driver.findElement(By.className("ri"));
-        driver.quit();
     }
 
     @Test
+    public void testImage() throws InterruptedException {
+        driver.get(urlImage);
+        driver.findElement(By.className("ri"));
+    }
+
+
+    @Test
     public void testVideo() throws InterruptedException {
-        driver = new FirefoxDriver();
         driver.get(urlVideo);
         driver.findElement(By.className("LM_video"));
-        driver.quit();
     }
 
     @Test
     public void testHTML() throws InterruptedException {
-        driver = new FirefoxDriver();
         driver.get(urlHtml);
         driver.findElement(By.className("LM_html_container"));
+    }
+
+    @Test
+    public void testLogin() throws InterruptedException {
+        driver.get(urlLogin);
+        driver.findElement(By.id("email")).sendKeys("superadmin@l.me");
+        driver.findElement(By.id("password")).sendKeys("1234");
+        driver.findElement(By.className("btn")).click();
+        Assert.assertFalse("cannot Login", driver.getPageSource().contains(cannotLoginMessage));
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
         driver.quit();
     }
 }
